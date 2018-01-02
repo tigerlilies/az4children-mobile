21c438a05769feca11107c5a98785f87c8294e39
lukitos <lukitos@yahoo.com>
2018-01-02 14:00:42 -0700

    Add form validation

--- components/Confirm.js
+++ components/Confirm.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View, Text, Linking } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as utilAction from '../actions/utils';
 import config from '../config/config';
-import { Button, FormLabel, FormInput } from 'react-native-elements';
+import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 const { EMAIL_SENDTO,
   EMAIL_SUBJECT,
@@ -13,66 +13,83 @@ const { EMAIL_SENDTO,
 
 let mentorEmail = '';
 
-const processSubmit = (props) => {
-  console.log('in processSubmit >>> mentorEmail:', mentorEmail);
-  const { navigate } = props.navigation;
-  let { id,
-    firstname,
-    lastname } = props.navigation.state.params;
-  utilAction.sendEmail({
-  	to: EMAIL_SENDTO,
-    subject: EMAIL_SUBJECT,
-    text: EMAIL_INTRO1,
-    html: `<p>${EMAIL_INTRO1}: ${mentorEmail}</p>
-      <p>${EMAIL_INTRO2}:</p>
-      <p>ID: ${id}</p>
-      <p>First Name: ${firstname}</p>
-      <p>Last Name: ${lastname}</p>`
-  });
-  navigate('Home');
-}
+class Confirm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: ''
+    };
+  }
 
-const Confirm = (props) => {
-  console.log('in Confirm >>> props', props);
-  let {
-    id,
-    age,
-    gender,
-    summary,
-    need1,
-    need2,
-    need3,
-    characteristic1,
-    characteristic2,
-    characteristic3 } = props.navigation.state.params;
-  let g = gender === 'M' ? 'Boy' : 'Girl';
-  return (
-    <View style={styles.container}>
-      <FormLabel>Email address:</FormLabel>
-      <FormInput
-        placeholder="Please enter your email address"
-        onChangeText={val => mentorEmail = val}
-        autoCapitalize="none"
-      />
-      <Text style={{paddingTop: 40}}>Thank you for selecting a {age} years old {g} to mentor.</Text>
-      <Text style={styles.summaryStyle}>Summary:</Text>
-      <Text>{summary}</Text>
-      <Text style={styles.spacer}>
-        If you have not filled out an application, please click on the link below:
-      </Text>
-      <Text style={{color: 'blue'}}
-        onPress={() => Linking.openURL('http://arizonansforchildren.org/page/volunteer-application')}>
-        Volunteer Application Form
-      </Text>
-      <Button
-        icon={{name: 'email'}}
-        backgroundColor='#523F78'
-        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginTop: 50, marginBottom: 100}}
-        title='SUBMIT'
-        onPress={() => processSubmit(props)}
-      />
-    </View>
-  );
+  processSubmit(props) {
+    console.log('in processSubmit >>> mentorEmail:', mentorEmail);
+    if (mentorEmail === '') {
+      this.setState({errorMessage: 'Email address is required!'});
+    } else {
+      const { navigate } = props.navigation;
+      let { id,
+        firstname,
+        lastname } = props.navigation.state.params;
+      utilAction.sendEmail({
+      	to: EMAIL_SENDTO,
+        subject: EMAIL_SUBJECT,
+        text: EMAIL_INTRO1,
+        html: `<p>${EMAIL_INTRO1}: ${mentorEmail}</p>
+          <p>${EMAIL_INTRO2}:</p>
+          <p>ID: ${id}</p>
+          <p>First Name: ${firstname}</p>
+          <p>Last Name: ${lastname}</p>`
+      });
+      navigate('Home');
+    }
+  }
+
+  render() {
+    console.log('in Confirm >>> props', this.props);
+    let {
+      id,
+      age,
+      gender,
+      summary,
+      need1,
+      need2,
+      need3,
+      characteristic1,
+      characteristic2,
+      characteristic3 } = this.props.navigation.state.params;
+    let g = gender === 'M' ? 'Boy' : 'Girl';
+
+    return (
+      <View style={styles.container}>
+        <FormLabel>Email address:</FormLabel>
+        <FormInput
+          placeholder="Please enter your email address"
+          onChangeText={val => mentorEmail = val}
+          autoCapitalize="none"
+        />
+        <FormValidationMessage>{this.state.errorMessage}</FormValidationMessage>
+        <Text style={{paddingTop: 20}}>Thank you for selecting a {age} years old {g} to mentor.</Text>
+        <Text style={styles.summaryStyle}>Summary:</Text>
+        <Text>{summary}</Text>
+        <Text style={styles.spacer}>
+          If you have not filled out an application, please click on the link below:
+        </Text>
+        <Text style={{color: 'blue'}}
+          onPress={() => Linking.openURL('http://arizonansforchildren.org/page/volunteer-application')}>
+          Volunteer Application Form
+        </Text>
+        <Button
+          icon={{name: 'email'}}
+          backgroundColor='#523F78'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginTop: 50, marginBottom: 100}}
+          title='SUBMIT'
+          onPress={() => this.processSubmit(this.props)}
+        />
+      </View>
+    );
+
+  }
+
 }
 
 function mapDispatchToProps(dispatch) {
