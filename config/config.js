ea549b1a7943f6a3732a0eb13fbe269fb8bcd64b
lukitos <lukitos@yahoo.com>
2018-08-27 10:33:11 -0700

    Regenerate ios files

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   API_URL: 'https://az4children-api.herokuapp.com',
-  EMAIL_SENDTO: 'az4children.laura@gmail.com',
+  EMAIL_SENDTO: 'tigerliliesnp@gmail.com',
   EMAIL_SUBJECT: 'Mentor selection',
   EMAIL_INTRO1: 'Mentor info',
   EMAIL_INTRO2: 'Mentor has selected'
